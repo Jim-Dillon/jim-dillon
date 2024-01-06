1acf9143c09e43cbfde32b6a81ce49c2f90205b1
Jim-Dillon <james.dillon94@gmail.com>
2024-01-06 17:55:22 +0000

    First commit - home page

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("js");
+    eleventyConfig.addPassthroughCopy("img");
+    // eleventyConfig.addPassthroughCopy("./src/CNAME")
+    eleventyConfig.addPassthroughCopy("css");
+    return {
+        dir: {
+            layouts: "_includes",
+        },
+    };
+};
