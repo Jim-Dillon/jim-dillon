59d7d32f48d3eac7b2f08ea08967be85f9a3d55d
Jim-Dillon <james.dillon94@gmail.com>
2024-01-15 10:35:23 +0000

    change layout file to njk

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("js");
     eleventyConfig.addPassthroughCopy("img");
     // eleventyConfig.addPassthroughCopy("./src/CNAME")
-    // eleventyConfig.setBrowserSyncConfig({
-    //     server: {
-    //         baseDir: "_site",
-    //         serverStaticOptions: {
-    //             base: "/jim-dillon/",
-    //         },
-    //     },
-    // });
     eleventyConfig.addPassthroughCopy("css");
     return {
         dir: {
