a21fd954cf2cca0bb931c2ffaa8f82df84e47abc
Jim-Dillon <james.dillon94@gmail.com>
2024-01-15 09:32:54 +0000

    set baseURL for project

--- .eleventy.js
+++ .eleventy.js
@@ -2,10 +2,18 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("js");
     eleventyConfig.addPassthroughCopy("img");
     // eleventyConfig.addPassthroughCopy("./src/CNAME")
+    eleventyConfig.setBrowserSyncConfig({
+        server: {
+            baseDir: "_site",
+            serverStaticOptions: {
+                base: "/jim-dillon/",
+            },
+        },
+    });
     eleventyConfig.addPassthroughCopy("css");
     return {
         dir: {
             layouts: "_includes",
         },
-    }
-}
+    };
+};
