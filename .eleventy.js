d02733e17891352cdeda4af4cb89f570a4d2d6cd
Jim-Dillon <james.dillon94@gmail.com>
2024-01-15 21:59:56 +0000

    change config file

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/img");
     // eleventyConfig.addPassthroughCopy("./src/CNAME")
     eleventyConfig.addPassthroughCopy("./src/css");
+    eleventyConfig.setBrowserSyncConfig({
+        open: true,
+        server: { baseDir: '_site', serveStaticOptions: { base: '/jim-dillon' } }
+    });
     return {
         dir: {
             input: "src",
