6f39640472635bc07601f77bc1b31b7fcfeec89c
Jim-Dillon <james.dillon94@gmail.com>
2024-02-05 14:09:37 +0000

    add tsconfig file and init ts

--- .eleventy.js
+++ .eleventy.js
@@ -3,20 +3,11 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/img");
     eleventyConfig.addPassthroughCopy("src/CNAME")
     eleventyConfig.addPassthroughCopy("./src/CSS");
-    // eleventyConfig.setBrowserSyncConfig({
-    //     open: true,
-    //     server: { 
-    //         baseDir: '_site', 
-    //         serveStaticOptions: { 
-    //             baseURL: '/jim-dillon' } 
-    //         }
-    // });
     return {
         dir: {
             input: "src",
             layouts: "_includes",
             output: "_site",
-            // pathPrefix: "/jim-dillon/"
         },
     };
     
